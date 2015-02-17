86fd21b6499fff4787f8ad7a84f99a47cf893058
Christian Rubiales <christianrubiales@yahoo.com>
2015-02-18 00:30:17 +0800

    IntroToTutorialChallenges

---
+++ src/arraysAndSorting/IntroToTutorialChallenges.java
@@ -0,0 +1,20 @@
+package arraysAndSorting;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class IntroToTutorialChallenges {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int V = in.nextInt();
+		int n = in.nextInt();
+		int[] ar = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			ar[i] = in.nextInt();
+		}
+		System.out.println(Arrays.binarySearch(ar, V));
+	}
+
+}
